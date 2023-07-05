c886b33a3f57668f5f217726fa97ebe5910cbbe6
Calos01 <ca_los01@hotmail.com>
2023-07-04 19:50:06 -0500

    solucion bootstrap y estructura componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VentasComponent } from './sections/ventas/ventas.component';
+import { PedidosComponent } from './sections/pedidos/pedidos.component';
+import { HealthComponent } from './sections/health/health.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'ventas',component:VentasComponent},
+  {path:'pedidos',component:PedidosComponent},
+  {path:'health',component:HealthComponent},
+  {path:'', redirectTo:'/ventas', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
