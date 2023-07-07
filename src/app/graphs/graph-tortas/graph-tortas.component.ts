175fa3c755a35b47f775fb0adb4442a3d46b8b0d
Calos01 <ca_los01@hotmail.com>
2023-07-06 20:03:49 -0500

    grafico torta

--- src/app/graphs/graph-tortas/graph-tortas.component.ts
+++ src/app/graphs/graph-tortas/graph-tortas.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit} from '@angular/core';
+import { Chart,registerables} from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-graph-tortas',
   templateUrl: './graph-tortas.component.html',
   styleUrls: ['./graph-tortas.component.css']
 })
-export class GraphTortasComponent {
-
+export class GraphTortasComponent implements OnInit,AfterViewInit{
+chart!:any;
+constructor() {
+  
+}
+  ngOnInit(): void {
+    this.renderGrafico();
+  }
+  ngAfterViewInit(): void {
+    this.resizeChart();
+  }
+  renderGrafico(){
+   this.chart=new Chart("torta",{
+      type: 'doughnut',
+      data: {
+        labels: [
+          'Red',
+          'Blue',
+          'Yellow'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ]
+        }]
+      }
+    });
+  }
+  //Nos sirve para que no se pierda los graficos al achicar y agrandar la pantalla
+  resizeChart() {
+    if (this.chart) {
+      this.chart.resize();
+    }
+  }
+  //Nos sirve para que no se pierda los graficos al achicar y agrandar la pantalla
+  @HostListener('window:resize')
+  onWindowResize() {
+    this.resizeChart();
+  }
 }
