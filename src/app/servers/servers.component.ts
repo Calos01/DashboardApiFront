d73af5f8fbecfb2aa3f2954426e33c075a66c4b4
Calos01 <ca_los01@hotmail.com>
2023-07-08 22:27:38 -0500

    server component listo

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Server } from '../Models/server';
 
 @Component({
@@ -6,6 +6,32 @@ import { Server } from '../Models/server';
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit{
   @Input() serverInput!:Server;
+  color!:string;
+  nametoogle!:string;
+  constructor() {
+    
+  }
+  ngOnInit(): void {
+    this.cambiarColor(this.serverInput.online)
+  }
+  
+  cambiarColor(online:boolean){
+    if(online){
+      this.color="green";
+      this.nametoogle="true"
+    }else{
+      this.color="red";
+      this.nametoogle="false"
+    }
+  }
+
+  buttonToggle(online:boolean){
+    console.log(online);
+    this.serverInput.online=!online
+    this.cambiarColor(!online)
+  }
+
+
 }
